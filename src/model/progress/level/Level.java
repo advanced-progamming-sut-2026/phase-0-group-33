cd16212296afeb37204b5352df45e4f4a7bd8931
AmirHossein Yousefi <a.yousefi8057@gmail.com>
2026-06-08 23:26:08 +0330

    phase-0: setting level,user..

--- src/model/progress/level/Level.java
+++ src/model/progress/level/Level.java
@@ -1,5 +1,39 @@
 package model.progress.level;
 
+import model.progress.chapter.Chapter;
+
 public abstract class Level {
-    
+
+    protected Chapter chapter;
+    protected int levelNumber;
+
+    public Level(Chapter chapter, int levelNumber) {
+        this.chapter = chapter;
+        this.levelNumber = levelNumber;
+    }
+
+    public void start() {
+        // TODO
+    }
+
+    public boolean checkVictoryCondition() {
+        // TODO
+        return false;
+    }
+
+    public Chapter getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(Chapter chapter) {
+        this.chapter = chapter;
+    }
+
+    public int getLevelNumber() {
+        return levelNumber;
+    }
+
+    public void setLevelNumber(int levelNumber) {
+        this.levelNumber = levelNumber;
+    }
 }
--- src/model/user/UserPlant.java
+++ src/model/user/UserPlant.java
@@ -1,5 +1,34 @@
 package model.user;
 
+import model.entitie.plant.PlantStats;
+import model.entitie.plant.PlantType;
+
 public class UserPlant {
-    
+
+    private PlantType plantType;
+    private int upgradeLevel = 1;
+
+    public UserPlant(PlantType plantType) {
+        this.plantType = plantType;
+    }
+
+    public PlantStats getUpgradedStats() {
+        return null;
+    }
+
+    public void upgrade() {
+        this.upgradeLevel++;
+    }
+
+    public PlantType getPlantType() {
+        return plantType;
+    }
+
+    public void setPlantType(PlantType plantType) {
+        this.plantType = plantType;
+    }
+
+    public int getUpgradeLevel() {
+        return upgradeLevel;
+    }
 }
