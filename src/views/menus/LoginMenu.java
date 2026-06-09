49d30210179c4a698a00487f1d10ed5d87df684a
AmirHossein Yousefi <a.yousefi8057@gmail.com>
2026-06-10 00:16:00 +0330

    phase-1: LoginMeniu

---
+++ src/views/menus/LoginMenu.java
@@ -0,0 +1,22 @@
+package views.menus;
+
+import controllers.menuControllers.LoginController;
+
+public class LoginMenu implements AppMenu {
+    private final LoginController controller;
+
+    public LoginMenu(LoginController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void display() {
+        // TODO
+    }
+
+    @Override
+    public String processCommand(String cmd) {
+        // TODO
+        return null;
+    }
+}
