09fb85aaac6d87077fce810e16149314b8556871
AmirHossein Yousefi <a.yousefi8057@gmail.com>
2026-06-10 00:17:13 +0330

    phase-1: ShopMenu

---
+++ src/views/menus/ShopMenu.java
@@ -0,0 +1,22 @@
+package views.menus;
+
+import controllers.menuControllers.ShopController;
+
+public class ShopMenu implements AppMenu {
+    private final ShopController controller;
+
+    public ShopMenu(ShopController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void display() {
+        // TODO
+    }
+
+    @Override
+    public String processCommand(String cmd) {
+        // TODO
+        return null;
+    }
+}
