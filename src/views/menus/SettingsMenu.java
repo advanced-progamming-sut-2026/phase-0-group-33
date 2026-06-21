c94352ab0f7ef3b52c069c9332494403efb5f60d
AmirHossein Yousefi <a.yousefi8057@gmail.com>
2026-06-21 22:48:57 +0330

    setting

--- src/views/menus/SettingsMenu.java
+++ src/views/menus/SettingsMenu.java
@@ -1,6 +1,11 @@
 package views.menus;
 
 import controllers.menuControllers.SettingsController;
+import models.Result;
+import models.enums.regexes.commandHandlers.GlobalCommands;
+import models.enums.regexes.commandHandlers.SettingsCommands;
+
+import java.util.regex.Matcher;
 
 public class SettingsMenu implements AppMenu {
     private final SettingsController controller;
@@ -11,7 +16,37 @@ public class SettingsMenu implements AppMenu {
 
     @Override
     public boolean processCommand(String cmd) {
-        // TODO
+        String input = cmd.trim();
+
+        Matcher changeDifficultyMatcher = SettingsCommands.CHANGE_DIFFICULTY.pattern.matcher(input);
+        if (changeDifficultyMatcher.matches()) {
+            String level = changeDifficultyMatcher.group("level");
+            // TODO
+            return true;
+        }
+
+        Matcher changeMenuMatcher = GlobalCommands.CHANGE_MENU.pattern.matcher(input);
+        if (changeMenuMatcher.matches()) {
+            String menu = changeMenuMatcher.group("menu");
+            // TODO
+            return true;
+        }
+
+        Matcher showMenuMatcher = GlobalCommands.SHOW_MENU.pattern.matcher(input);
+        if (showMenuMatcher.matches()) {
+            Result result = new Result();
+            result.setSuccess(true);
+            result.addMessage("Settings menu");
+            printResultMsg(result);
+            return true;
+        }
+
+        Matcher exitMatcher = GlobalCommands.EXIT.pattern.matcher(input);
+        if (exitMatcher.matches()) {
+            // TODO
+            return true;
+        }
+
         return false;
     }
 }
