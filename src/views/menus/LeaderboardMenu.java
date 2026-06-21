95314d0cd8d6ba5ec784157b384bffdb9bb73275
AmirHossein Yousefi <a.yousefi8057@gmail.com>
2026-06-21 22:52:16 +0330

    leaderboard

--- src/views/menus/LeaderboardMenu.java
+++ src/views/menus/LeaderboardMenu.java
@@ -1,6 +1,10 @@
 package views.menus;
 
 import controllers.menuControllers.LeaderboardController;
+import models.Result;
+import models.enums.regexes.commandHandlers.GlobalCommands;
+
+import java.util.regex.Matcher;
 
 public class LeaderboardMenu implements AppMenu {
     private final LeaderboardController controller;
@@ -11,7 +15,30 @@ public class LeaderboardMenu implements AppMenu {
 
     @Override
     public boolean processCommand(String cmd) {
-        // TODO
+        String input = cmd.trim();
+
+        Matcher changeMenuMatcher = GlobalCommands.CHANGE_MENU.pattern.matcher(input);
+        if (changeMenuMatcher.matches()) {
+            String menu = changeMenuMatcher.group("menu");
+            // TODO
+            return true;
+        }
+
+        Matcher showMenuMatcher = GlobalCommands.SHOW_MENU.pattern.matcher(input);
+        if (showMenuMatcher.matches()) {
+            Result result = new Result();
+            result.setSuccess(true);
+            result.addMessage("Leaderboard menu");
+            printResultMsg(result);
+            return true;
+        }
+
+        Matcher exitMatcher = GlobalCommands.EXIT.pattern.matcher(input);
+        if (exitMatcher.matches()) {
+            // TODO
+            return true;
+        }
+
         return false;
     }
 }
