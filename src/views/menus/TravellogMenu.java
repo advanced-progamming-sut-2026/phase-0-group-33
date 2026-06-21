d486a9e926eba3cb6a4910465e0163f51cb7e132
AmirHossein Yousefi <a.yousefi8057@gmail.com>
2026-06-21 22:51:53 +0330

    travellog

--- src/views/menus/TravellogMenu.java
+++ src/views/menus/TravellogMenu.java
@@ -1,6 +1,11 @@
 package views.menus;
 
 import controllers.menuControllers.TravelLogController;
+import models.Result;
+import models.enums.regexes.commandHandlers.GlobalCommands;
+import models.enums.regexes.commandHandlers.TravellogCommands;
+
+import java.util.regex.Matcher;
 
 public class TravellogMenu implements AppMenu {
     private final TravelLogController controller;
@@ -11,7 +16,37 @@ public class TravellogMenu implements AppMenu {
 
     @Override
     public boolean processCommand(String cmd) {
-        // TODO
+        String input = cmd.trim();
+
+        Matcher travelLogPageMatcher = TravellogCommands.TRAVEL_LOG_PAGE.pattern.matcher(input);
+        if (travelLogPageMatcher.matches()) {
+            String pageName = travelLogPageMatcher.group("pageName");
+            // TODO
+            return true;
+        }
+
+        Matcher changeMenuMatcher = GlobalCommands.CHANGE_MENU.pattern.matcher(input);
+        if (changeMenuMatcher.matches()) {
+            String menu = changeMenuMatcher.group("menu");
+            // TODO
+            return true;
+        }
+
+        Matcher showMenuMatcher = GlobalCommands.SHOW_MENU.pattern.matcher(input);
+        if (showMenuMatcher.matches()) {
+            Result result = new Result();
+            result.setSuccess(true);
+            result.addMessage("Travellog menu");
+            printResultMsg(result);
+            return true;
+        }
+
+        Matcher exitMatcher = GlobalCommands.EXIT.pattern.matcher(input);
+        if (exitMatcher.matches()) {
+            // TODO
+            return true;
+        }
+
         return false;
     }
 }
