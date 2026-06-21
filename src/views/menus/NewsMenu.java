46668d0b44753b0f03f0ae02b80a5fb7661b5fb2
AmirHossein Yousefi <a.yousefi8057@gmail.com>
2026-06-21 22:49:25 +0330

    news

--- src/views/menus/NewsMenu.java
+++ src/views/menus/NewsMenu.java
@@ -1,6 +1,11 @@
 package views.menus;
 
 import controllers.menuControllers.NewsController;
+import models.Result;
+import models.enums.regexes.commandHandlers.GlobalCommands;
+import models.enums.regexes.commandHandlers.NewsCommands;
+
+import java.util.regex.Matcher;
 
 public class NewsMenu implements AppMenu {
     private final NewsController controller;
@@ -11,7 +16,42 @@ public class NewsMenu implements AppMenu {
 
     @Override
     public boolean processCommand(String cmd) {
-        // TODO
+        String input = cmd.trim();
+
+        Matcher showUnreadMatcher = NewsCommands.SHOW_UNREAD.pattern.matcher(input);
+        if (showUnreadMatcher.matches()) {
+            // TODO
+            return true;
+        }
+
+        Matcher showAllMatcher = NewsCommands.SHOW_ALL.pattern.matcher(input);
+        if (showAllMatcher.matches()) {
+            // TODO
+            return true;
+        }
+
+        Matcher changeMenuMatcher = GlobalCommands.CHANGE_MENU.pattern.matcher(input);
+        if (changeMenuMatcher.matches()) {
+            String menu = changeMenuMatcher.group("menu");
+            // TODO
+            return true;
+        }
+
+        Matcher showMenuMatcher = GlobalCommands.SHOW_MENU.pattern.matcher(input);
+        if (showMenuMatcher.matches()) {
+            Result result = new Result();
+            result.setSuccess(true);
+            result.addMessage("News menu");
+            printResultMsg(result);
+            return true;
+        }
+
+        Matcher exitMatcher = GlobalCommands.EXIT.pattern.matcher(input);
+        if (exitMatcher.matches()) {
+            // TODO
+            return true;
+        }
+
         return false;
     }
 }
